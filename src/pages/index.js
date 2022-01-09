934befee8cc55b47fa2e671a3d86c4914e6d2569
lcsoares10 <lucassoares81@hotmail.com>
2022-01-09 14:16:12 -0300

    refactor(home): add componenents main and anside

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,21 @@
-import styled from "styled-components";
 import Theme from "../theme";
 import GlobalStyles from "../css/global.js";
 
-import Header from '../components/Header'
-
-const Main = styled.div`
-  background-color: ${(props) => props.theme.colors.primary};
-`;
+import Aside from "../components/Aside";
+import Header from "../components/Header";
+import Main from "../components/Main";
+import Row from "../components/Row";
 
 export default function Home() {
   return (
     <Theme>
-    <Header/>
-      <Main>
-        <h1>Hello</h1>
-      </Main>
+      <Header />
+
+      <Row>
+        <Main />
+        <Aside />
+      </Row>
+
       <GlobalStyles />
     </Theme>
   );
