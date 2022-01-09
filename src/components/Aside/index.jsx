df813eff250ac2a200711fc8578d3717980b08a6
lcsoares10 <lucassoares81@hotmail.com>
2022-01-09 15:50:55 -0300

    style(aside): defined style

--- src/components/Aside/index.jsx
+++ src/components/Aside/index.jsx
@@ -5,14 +5,35 @@ function Aside({ children }) {
   return (
     <S.Aside>
       {children}
-      <h2>Detalhes Climaticos</h2>
-      <p>Nuvens</p>
-      <p>Precipitação</p>
-      <p>Humidade</p>
-      <p>Vento</p>
-      <h3>Temperatura</h3>
-      <span>25</span>
-      <span>26</span>
+      <S.Section>
+        <h2>Detalhes Climaticos</h2>
+        <S.Article>
+          <S.TextLine>
+            <p>Nuvens</p>
+            <span>100%</span>
+          </S.TextLine>
+
+          <S.TextLine>
+            <p>Precipitação</p>
+            <span>100%</span>
+          </S.TextLine>
+
+          <S.TextLine>
+            <p>Humidade</p>
+            <span>100%</span>
+          </S.TextLine>
+
+          <S.TextLine>
+            <p>Vento</p>
+            <span>100%</span>
+          </S.TextLine>
+        </S.Article>
+        <S.Article>
+          <h3>Temperatura</h3>
+          <span>25</span>
+          <span>26</span>
+        </S.Article>
+      </S.Section>
     </S.Aside>
   );
 }
--- src/components/Aside/style.js
+++ src/components/Aside/style.js
@@ -6,4 +6,31 @@ export const Aside = styled.div`
   background-color: red;
   height: 100vh;
   background-color: ${(props) => props.theme.colors.primary};
+  box-shadow: -4px 5px 12px -4px #000000;
+  z-index: 3;
+  p,
+  span {
+    color: ${(props) => props.theme.colors.secondary};
+    font-size: ${(props) => props.theme.fontSizes.medium};
+  }
+`;
+
+export const TextLine = styled.div`
+  margin: 25px 0px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const Section = styled.div`
+  padding: 20px;
+  h3,
+  h2 {
+    font-weight: 300;
+    margin: 10px 0px;
+    color: ${(props) => props.theme.colors.color};
+  }
+`;
+
+export const Article = styled.div`
+  margin: 20px 0px;
 `;
