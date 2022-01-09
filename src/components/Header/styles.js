20a85055032b7a2fdf82a623bee5e833c6fe5e2f
lcsoares10 <lucassoares81@hotmail.com>
2022-01-09 16:29:24 -0300

    feat(header): add box shadow

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -2,12 +2,14 @@ import styled from "styled-components";
 
 export const Header = styled.header`
   width: 100%;
-  max-width: 1400px;
+
   min-height: 30px;
   display: flex;
   flex-direction: row;
   justify-content: space-between;
   padding: 10px;
+  box-shadow: 1px 4px 14px -4px #000000;
+  z-index: 3;
 `;
 
 export const Text = styled.p`
