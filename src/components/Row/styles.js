5122a57a8d3f3b1cf1078262b53f782a50c91618
lcsoares10 <lucassoares81@hotmail.com>
2022-01-09 19:03:23 -0300

    style(global): ajust responsive

--- src/components/Row/styles.js
+++ src/components/Row/styles.js
@@ -2,8 +2,8 @@ import styled from "styled-components";
 
 export const Row = styled.div`
   display: flex;
-  flex-wrap: wrap;
-  @media (max-width: 800px) {
+  flex-wrap: nowrap;
+  @media (max-width: ${({ theme }) => theme.media.tablet}) {
     flex-direction: column;
   }
 `;
