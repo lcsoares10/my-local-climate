cced817e64311f488a70ddeb941cafac650eb9b5
lcsoares10 <lucassoares81@hotmail.com>
2022-01-10 21:33:19 -0300

    style(main): ajust layout

--- src/components/Main/styles.js
+++ src/components/Main/styles.js
@@ -10,7 +10,7 @@ export const Main = styled.div`
 export const Section = styled.div`
   position: relative;
   width: 100%;
-  height: 100vh;
+  height: calc(100vh - 35px);
   @media (max-width: ${({ theme }) => theme.media.tablet}) {
     height: 55vh;
   }
@@ -18,11 +18,17 @@ export const Section = styled.div`
 
 export const Mask = styled.div`
   width: 100%;
-  height: 100vh;
+  height: 100%;
   position: absolute;
   z-index: 1;
   background-color: rgba(0, 0, 0, 0.6);
   @media (max-width: ${({ theme }) => theme.media.tablet}) {
     height: 55vh;
   }
+`;
+export const Article = styled.div`
+  padding: 20px;
+  position: absolute;
+  bottom: 0px;
+  width: 100%;
 `;
