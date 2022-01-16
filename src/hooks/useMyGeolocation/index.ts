e7083d1341d5b06b97331464503d1d588a36d013
lcsoares10 <lucassoares81@hotmail.com>
2022-01-16 20:26:19 -0300

    feat(hook): create hook for get geolocation

---
+++ src/hooks/useMyGeolocation/index.ts
@@ -0,0 +1,25 @@
+type TCoordinates = {
+  latitude?: Number;
+  longitude?: Number;
+};
+
+import { useState, useEffect } from "react";
+
+export default function  useMyGeolocation() {
+  const [coords, setCoords] = useState<TCoordinates>({ longitude:0, latitude:0 });
+  const [loading, setLoading] = useState(true);
+
+  const success = async (position) => {
+    const { longitude, latitude } = await position.coords;
+    setCoords({ longitude, latitude });
+    setLoading(false)
+  };
+  const error = (err) => console.warn(`ERROR(${err.code}): ${err.message}`);
+
+  useEffect(() => {
+    const handleGetGeolocation = () => navigator.geolocation.getCurrentPosition(success, error);
+    handleGetGeolocation();
+  },[]);
+  
+  return {coords,loading};
+}
