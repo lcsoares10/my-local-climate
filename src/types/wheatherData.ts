6fa1870a5d0fd52992e51f49aa41b272d421c8d9
lcsoares10 <lucassoares81@hotmail.com>
2022-01-17 00:04:47 -0300

    feat(types and interfaces): created types and interfaces of response data

---
+++ src/types/wheatherData.ts
@@ -0,0 +1,70 @@
+export interface IWeatherResponse {
+  current: ICurrentWeather;
+  daily: Array<IDailyWeather>;
+  lat: number;
+  lon: number;
+  timezone: string;
+  timezone_offset: number;
+}
+
+export type TWeather = {
+  description: string;
+  icon: string;
+  id: number;
+  main: string;
+};
+
+export interface ICurrentWeather {
+  clouds: number;
+  dew_point: number;
+  dt: number;
+  feels_like: number;
+  humidity: number;
+  pressure: number;
+  sunrise: number;
+  sunset: number;
+  temp: number;
+  uvi: number;
+  visibility: number;
+  weather: Array<TWeather>;
+  wind_deg: number;
+  wind_speed: number;
+}
+
+export interface IDailyWeather {
+  clouds: number;
+  dew_point: number;
+  dt: number;
+  feels_like: TFeelsLike;
+  humidity: number;
+  moon_phase: number;
+  moonrise: number;
+  moonset: number;
+  pop: number;
+  pressure: number;
+  rain: number;
+  sunrise: number;
+  sunset: number;
+  temp: TTemp;
+  uvi: number;
+  weather: Array<TWeather>;
+  wind_deg: number;
+  wind_gust: number;
+  wind_speed: number;
+}
+
+export type TFeelsLike = {
+  day: number;
+  eve: number;
+  morn: number;
+  night: number;
+};
+
+export type TTemp = {
+  day: number;
+  eve: number;
+  max: number;
+  min: number;
+  morn: number;
+  night: number;
+};
