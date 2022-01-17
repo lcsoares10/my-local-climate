140cf24a1b7caeaeca7ec1d76fad4d517d1358f9
lcsoares10 <lucassoares81@hotmail.com>
2022-01-17 14:11:40 -0300

    feat(wheaterprovider): add types and state for daily

--- src/context/weatherProvider/index.tsx
+++ src/context/weatherProvider/index.tsx
@@ -1,28 +1,45 @@
 import React, { createContext, useMemo, useState, useEffect } from "react";
-import useSWR from "swr";
+import useSWR,{SWRResponse} from "swr";
 import { fetcher } from "../../services/api";
 import useMyGeolocation from "../../hooks/useMyGeolocation";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.min.css";
+import {IWeatherResponse,ICurrentWeather,IDailyWeather} from '../../types/wheatherData'
 
-const WeatherContext = createContext({});
+type TPropsContext = {
+  weatherCurrent: ICurrentWeather;
+  weatherDaily: Array<IDailyWeather>;
+}
+
+const DefaultValue = {
+  weatherCurrent: null,
+  weatherDaily: null
+}
+
+const WeatherContext = createContext<TPropsContext>(DefaultValue);
 
 function WeatherProvider({ children }) {
-  const [weatherCurrent, setWeatherCurrent] = useState();
+  const [weatherCurrent, setWeatherCurrent] = useState<ICurrentWeather>();
+  const [weatherDaily, setWeatherDaily] = useState<Array<IDailyWeather>>();
 
   const { coords, loading } = useMyGeolocation();
-  const { data, error } = useSWR(
+  const { data, error }:SWRResponse<IWeatherResponse> = useSWR(
     !loading ? `onecall?lat=${coords.latitude}&lon=${coords.longitude}` : null,
     fetcher
   );
 
-  const handleGetDataWeather = () => {
-    console.log(data)
-    setWeatherCurrent(data);
+  const handleDailyWeather = () => {
+    setWeatherDaily(data?.daily);
+  };
+
+  const handleGetDataWeatherCurrent = () => {
+
+    setWeatherCurrent(data?.current);
   };
 
   useEffect(() => {
-    handleGetDataWeather();
+    handleGetDataWeatherCurrent();
+    handleDailyWeather();
   }, [data]);
 
   useEffect(() => {
@@ -40,9 +57,9 @@ function WeatherProvider({ children }) {
 
   const value = useMemo(
     () => ({
-      weatherCurrent,
+      weatherCurrent,weatherDaily
     }),
-    [weatherCurrent]
+    [weatherCurrent,weatherDaily]
   );
 
   return (
