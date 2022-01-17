68d031de52be857d8ddc1abef0f2ecdf31046bd0
lcsoares10 <lucassoares81@hotmail.com>
2022-01-16 23:25:22 -0300

    feat(service): create config axios for request

---
+++ src/services/api.ts
@@ -0,0 +1,21 @@
+
+import axios from 'axios'
+const urlApiWheater = process.env.NEXT_PUBLIC_URL_API || ''
+console.log(urlApiWheater)
+
+const appid = process.env.NEXT_PUBLIC_KEY_OPEN_WEATHER || ''
+
+const apiAxios = axios.create({
+    baseURL:urlApiWheater,
+    params:{appid,lang:'pt_br'},
+    
+});
+
+export const fetcher = (url) => apiAxios.get(url).then(res => res.data)
+
+
+
+
+
+
+
