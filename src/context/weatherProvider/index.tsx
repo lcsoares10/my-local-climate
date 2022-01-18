7dd37f37bf1da4c14dd234f99b78d7ef34dcd286
lcsoares10 <lucassoares81@hotmail.com>
2022-01-18 12:41:28 -0300

    feat(wheatherprovider): add function for merge data current and data daily

--- src/context/weatherProvider/index.tsx
+++ src/context/weatherProvider/index.tsx
@@ -1,45 +1,81 @@
 import React, { createContext, useMemo, useState, useEffect } from "react";
-import useSWR,{SWRResponse} from "swr";
+import useSWR, { SWRResponse } from "swr";
+import dayjs from "dayjs";
 import { fetcher } from "../../services/api";
 import useMyGeolocation from "../../hooks/useMyGeolocation";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.min.css";
-import {IWeatherResponse,ICurrentWeather,IDailyWeather} from '../../types/wheatherData'
+import {
+  IWeatherResponse,
+  ICurrentWeather,
+  IDailyWeather,
+} from "../../types/wheatherData";
+
+type TDailyWeatherFormat = Omit<IDailyWeather, "dt"> & { dt: String };
+type TCurrentWeatherFormat = Omit<ICurrentWeather, "dt"> & { dt: String };
 
 type TPropsContext = {
-  weatherCurrent: ICurrentWeather;
-  weatherDaily: Array<IDailyWeather>;
-}
+  weatherCurrent: TCurrentWeatherFormat;
+  weatherDaily: Array<TDailyWeatherFormat>;
+  getWeatherCurrentWithDaily: {
+    current: TCurrentWeatherFormat;
+    daily: TDailyWeatherFormat;
+  };
+};
 
 const DefaultValue = {
   weatherCurrent: null,
-  weatherDaily: null
-}
+  weatherDaily: null,
+  getWeatherCurrentWithDaily: null,
+};
 
 const WeatherContext = createContext<TPropsContext>(DefaultValue);
 
 function WeatherProvider({ children }) {
-  const [weatherCurrent, setWeatherCurrent] = useState<ICurrentWeather>();
-  const [weatherDaily, setWeatherDaily] = useState<Array<IDailyWeather>>();
+  const [weatherCurrent, setWeatherCurrent] = useState<TCurrentWeatherFormat>();
+  const [weatherDaily, setWeatherDaily] =
+    useState<Array<TDailyWeatherFormat>>();
 
   const { coords, loading } = useMyGeolocation();
-  const { data, error }:SWRResponse<IWeatherResponse> = useSWR(
+  const { data, error }: SWRResponse<IWeatherResponse> = useSWR(
     !loading ? `onecall?lat=${coords.latitude}&lon=${coords.longitude}` : null,
     fetcher
   );
 
-  const handleDailyWeather = () => {
-    setWeatherDaily(data?.daily);
+  const setDailyWeather = () => {
+    if(!!data?.daily){
+      const dailyFormat = data?.daily.map((daily) => {
+        return { ...daily, dt: dayjs.unix(daily.dt).format("DD/MM/YYYY") };
+      });
+      setWeatherDaily(dailyFormat);
+    }
+
+
+
   };
 
-  const handleGetDataWeatherCurrent = () => {
-
-    setWeatherCurrent(data?.current);
+  const setDataWeatherCurrent = () => {
+    if(!!data?.current){
+      setWeatherCurrent({
+        ...data?.current,
+        dt: dayjs.unix(data?.current.dt).format("DD/MM/YYYY"),
+      });
+    }
+ 
   };
+
+  const getWeatherCurrentWithDaily = useMemo(() => {
+
+    const dailyCurrent = weatherDaily?.find(
+      (daily) => daily.dt === weatherCurrent.dt
+    );
+
+    return { current: weatherCurrent, daily: dailyCurrent };
+  }, [weatherDaily, weatherCurrent]);
 
   useEffect(() => {
-    handleGetDataWeatherCurrent();
-    handleDailyWeather();
+    setDataWeatherCurrent();
+    setDailyWeather();
   }, [data]);
 
   useEffect(() => {
@@ -57,9 +93,11 @@ function WeatherProvider({ children }) {
 
   const value = useMemo(
     () => ({
-      weatherCurrent,weatherDaily
+      weatherCurrent,
+      weatherDaily,
+      getWeatherCurrentWithDaily,
     }),
-    [weatherCurrent,weatherDaily]
+    [weatherCurrent, weatherDaily, getWeatherCurrentWithDaily]
   );
 
   return (
