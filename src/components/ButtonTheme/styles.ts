da272d249552efd3fe90c47948fe1383ef19c91c
lcsoares10 <lucassoares81@hotmail.com>
2022-01-19 15:43:47 -0300

    style(theme): ajust

--- src/components/ButtonTheme/styles.ts
+++ src/components/ButtonTheme/styles.ts
@@ -8,9 +8,9 @@ export const Container = styled.div<ContainerProps>`
   background-color: ${(props) => props.type && props.theme.colors.primary};
   border-radius: 5px;
   border: 1px solid rgba(255, 255, 255, 0.5);
-  position: absolute;
-  top: 50px;
-  left: 10px;
+  position: fixed;
+  top: 40px;
+  left: 5px;
   z-index: 99;
   cursor: pointer;
   padding: 5px;
