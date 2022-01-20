85163667962cb219ca5882bd1677711a48f1e3fa
lcsoares10 <lucassoares81@hotmail.com>
2022-01-20 13:41:08 -0300

    test(weatherdetail): add tests

---
+++ src/components/Widgets/DetailWeather/index.test.tsx
@@ -0,0 +1,46 @@
+import { render, screen } from "@testing-library/react";
+import Theme from "../../../theme";
+import DetailWeather from ".";
+import { mockDetailWheater } from "./mock";
+
+describe("<DetailWeather />", () => {
+  it("should view in display weather details", async () => {
+    const { container, debug } = render(
+      <Theme>
+        <DetailWeather {...mockDetailWheater} />
+      </Theme>
+    );
+  
+
+    expect(screen.getByText(/Nuvens/i)).toBeInTheDocument()
+    expect(screen.getByText(/Precipitação/i)).toBeInTheDocument()
+    expect(screen.getByText(/Humidade/i)).toBeInTheDocument()
+    expect(screen.getByText(/Vento/i)).toBeInTheDocument()
+    expect(screen.getByText(/Índice UV/i)).toBeInTheDocument()
+
+    expect(screen.getByText(`${mockDetailWheater.pop}%`)).toBeInTheDocument();
+    expect(
+      screen.getByText(`${mockDetailWheater.clouds}%`)
+    ).toBeInTheDocument();
+    expect(
+      screen.getByText(`${mockDetailWheater.humidity}%`)
+    ).toBeInTheDocument();
+    expect(screen.getByText(`${mockDetailWheater.uvi}`)).toBeInTheDocument();
+    expect(
+      screen.getByText(`${(mockDetailWheater.wind_speed * 3.6).toFixed(1)} km/h`)
+    ).toBeInTheDocument();
+
+    expect(container.firstChild).toMatchSnapshot();
+  });
+
+  it("should view in display skeleton", async () => {
+    const { container, debug } = render(
+      <Theme>
+        <DetailWeather />
+      </Theme>
+    );
+
+    expect(screen.getByTestId("skeleton-detailweather")).toBeTruthy();
+
+  });
+});
---
+++ src/components/Widgets/DetailWeather/mock.ts
@@ -0,0 +1,9 @@
+
+
+export const mockDetailWheater = {
+  clouds: 10,
+  pop: 5,
+  wind_speed: 100,
+  humidity: 0,
+  uvi: 70,
+};
