a8a47417f7a967679c0d95fb8863d1639b7bf11b
lcsoares10 <lucassoares81@hotmail.com>
2022-01-21 16:57:26 -0300

    test: created mock for hook use MyGeolocation

--- jest.setup.js
+++ jest.setup.js
@@ -1,8 +1,18 @@
 import "@testing-library/jest-dom/extend-expect";
-jest.mock('next/image', () => ({
-    __esModule: true,
-    default: () => {
-      return (<div data-testid="image-mock">'image'</div>);
-    },
-  }));
+jest.mock("next/image", () => ({
+  __esModule: true,
+  default: () => {
+    return <div data-testid="image-mock">'image'</div>;
+  },
+}));
 
+jest.mock("./src/hooks/useMyGeolocation", () => ({
+  __esModule: true,
+  default: () => ({
+    coords: {
+      log: 111,
+      lat: 222,
+    },
+    loading: false,
+  }),
+}));
