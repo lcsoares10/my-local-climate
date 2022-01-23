a0b5e27cada45ddac845993f88eeb6299c6bcf17
lcsoares10 <lucassoares81@hotmail.com>
2022-01-23 13:58:13 -0300

    style: ajust

--- src/components/Aside/style.ts
+++ src/components/Aside/style.ts
@@ -4,15 +4,20 @@ export const Aside = styled.div`
   flex: 20%;
   z-index: 1;
   background-color: ${(props) => props.theme.colors.primary};
-
+  position: fixed;
   height: 100%;
   z-index: 3;
+  right: 0px;
+  width: 20%;
+
 
   @media (max-width: ${({ theme }) => theme.media.tablet}) {
     box-shadow: -4px 5px 12px -4px #000000;
     margin-top: -50px;
     border-top-left-radius: 40px;
     border-top-right-radius: 40px;
+    position: relative;
+    width: 100%;
   }
 `;
 
